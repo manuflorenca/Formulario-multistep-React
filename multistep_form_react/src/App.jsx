42d9b807bf41ee48fb8f304e909b194097eab779
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-01-09 12:13:53 -0300

    Falta fazer o componente de envio de avaliação

--- multistep_form_react/src/App.jsx
+++ multistep_form_react/src/App.jsx
@@ -4,6 +4,8 @@ import {FiSend} from 'react-icons/fi';
 import UserForm from './components/UserForm';
 import ReviewForm from './components/ReviewForm';
 import Thanks from './components/Thanks';
+import Steps from './components/Steps';
+
 
 // Hooks
 import { UseForm } from './hooks/useForm';
@@ -27,7 +29,7 @@ function App() {
       <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto </p>
     </div>
     <div className="form-container">
-      <p>etapas</p>
+      <Steps currentStep={currentStep} />
       <form onSubmit={(e) => changeStep(currentStep +1, e )}>
         <div className="inputs-container">{currentComponent}</div>
         <div className="actions">
--- multistep_form_react/src/components/ReviewForm.jsx
+++ multistep_form_react/src/components/ReviewForm.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import  {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill
+}  from 'react-icons/bs';
+
+import './ReviewForm.css';
 
 const ReviewForm = () => {
   return (
-    <div>
-        <h2>Review Form</h2>
+    <div className='review-form'>
+        <div className="form-control score-container">
+          <label className="radio-container">
+            <input type="radio" value="unsatisfied" name='review' required />
+            < BsFillEmojiFrownFill/>
+            <p>Insatisfeito</p>
+          </label>
+          <label className="radio-container">
+            <input type="radio" value="neutral" name='review' required />
+            < BsFillEmojiNeutralFill/>
+            <p>Poderia ser melhor</p>
+          </label>
+          <label className="radio-container">
+            <input type="radio" value="satisfied" name='review' required />
+            < BsFillEmojiSmileFill/>
+            <p>Satisfeito</p>
+          </label>
+          <label className="radio-container">
+            <input type="radio" value="very_satisfied" name='review' required />
+            < BsFillEmojiHeartEyesFill/>
+            <p>Muito Satisfeito</p>
+          </label>
+        </div>
+        <div className="form-control">
+          <label htmlFor="comment"> Comentario:</label>
+          <textarea name="comment" id="comment" placeholder='Conte como foi sua experiencia com o produto...' required></textarea>
+        </div>
     </div>
   )
 }
